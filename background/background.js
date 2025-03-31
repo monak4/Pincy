735bbfec28151faf8b358ff0537cf51f91ea746d
monak4 <monakada210@gmail.com>
2025-03-31 14:11:22 +0900

    Add shortcuts

--- background/background.js
+++ background/background.js
@@ -1,7 +1,10 @@
-const browser = typeof browser !== "undefined" ? browser : chrome;
+const browserAPI = typeof browser !== "undefined" ? browser : chrome;
 
-browser.commands.onCommand.addListener((command) => {
-	if (command === "toggle-feature") {
+console.log("debug");
+
+browserAPI.commands.onCommand.addListener((command) => {
+	console.log("debug Toggling the feature!");
+	if (command === "toggle-popup") {
 		console.log("Toggling the feature!");
 	}
 });
