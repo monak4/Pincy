698d35d89972b62727adf7b516a5f8702a269a1f
monak4 <monakada210@gmail.com>
2025-04-02 12:34:22 +0900

    Fix btn color & size

--- popup/popup.js
+++ popup/popup.js
@@ -34,7 +34,7 @@ function loadSavedNotes() {
 				).toLocaleString("ja-JP")} | ${note.content.length}文字</p>
 				<button type="button" class="delete-pin-btn" data-note-id="${
 					note.id
-				}"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/></svg></button>
+				}"><svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#FFFFFF"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/></svg></button>
             `;
 
 			savedNotesContainer.appendChild(noteElement);
